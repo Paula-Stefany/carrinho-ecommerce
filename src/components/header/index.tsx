8a78daddaaf23df04df2a722e5f563af4e023cb7
Paula-Stefany <paula.sfa@outlook.com>
2025-04-08 22:00:27 -0300

    Header concluído

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,27 @@
+import { Link } from 'react-router'
+import { FiShoppingCart } from 'react-icons/fi'
+
+
 export function Header(){
     return(
-        <div>
-            <h1>HEADER COMPONENT</h1>
-        </div>
+        <header className='w-full border-b border-slate-300'>
+            <div className='w-full flex justify-center items-center py-1.5 bg-slate-900 text-amber-50'>
+                <p className='font-bold italic text-center text-xs'>Compre sem burocracia aqui no Guitar Shop! <span className='font-normal'>Promoções</span> todos os dias </p>
+            </div>
+
+            <nav className='w-full max-w-7xl h-22 items-center justify-between flex px-4 mx-auto' >
+                <Link className='font-bold text-xl md:text-2xl text-slate-900' to="/">
+                Guitar Shop
+                </Link>
+
+                <Link className='relative' to="/cart">
+                    <FiShoppingCart size={25} className='text-slate-900' />
+                    <span className='absolute -top-3 -right-3 px-2.5 bg-slate-300 opacity-90 rounded-full w-6 h-6 flex justify-center font-bold text-slate-900'>
+                        2
+                    </span>
+                </Link>
+            </nav>
+
+        </header>
     )
 }
