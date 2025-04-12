b15699849b6b9554a10c08026abbcc5a3a0c5864
Paula-Stefany <paula.sfa@outlook.com>
2025-04-12 07:11:18 -0300

    Adicionado segundo banner na Home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,6 @@
 import { Banner } from '../../components/banner' 
 import { Products } from '../../components/products'
+import { Banner2 } from '../../components/banner2'
 
 
 export function Home(){
@@ -8,6 +9,7 @@ export function Home(){
         <main className='w-full max-w-7xl px-4 m-auto'>
             <Banner/>
             <Products/>
+            <Banner2/>
         </main>
     )
 }
