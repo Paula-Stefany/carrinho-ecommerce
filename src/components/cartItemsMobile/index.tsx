4aa1d00a6d7ddaff8b453f6a1a6b48c77ff06d36
Paula-Stefany <paula.sfa@outlook.com>
2025-04-28 19:49:14 -0300

    Exibindo dados do contexto no carrinho mobile

--- src/components/cartItemsMobile/index.tsx
+++ src/components/cartItemsMobile/index.tsx
@@ -1,29 +1,46 @@
 import { RiDeleteBinLine } from 'react-icons/ri';
+import { CartProps } from '../../contexts/CartContext'
 
 
-export function CartItemsMobile(){
+
+interface CartItemsMobileProps{
+    cart: CartProps[];
+    addItemCart: (newItem: CartProps) => void;
+}
+
+export function CartItemsMobile({cart, addItemCart}: CartItemsMobileProps){
+
     return(
+        
         <div className='py-6 w-full block text-slate-900 items-center justify-between text-lg'>
-            <article className='h-30 flex  justify-between w-full border-b-2 border-b-slate-500 mb-5'>
-                <img src="https://www.wb.com.br/upload/produto/imagem/fone-de-ouvido-headphone-bluetooth-sem-fio-over-ear-wb-gyda-com-100-horas-de-bateria-case-incluso-25.webp" alt="Imagem do produto"/>
+            { cart.map((product) => (
+                <article className='h-30 flex  n w-full border-b-2 border-b-slate-500 mb-5' key={product.id}>
+                    <img src={product.cover} alt={product.title} className='w-30 mr-5'/>
 
-                <div className='flex flex-col justify-between py-3'>
-                    <div>R$200</div>
-                    <div className=' flex gap-2 items-center'>
-                        <button className='px-2.5 py-1 bg-slate-500 hover:bg-slate-600 cursor-pointer rounded font-bold text-xl'>+</button>
-                                2
-                        <button className='px-2.5 py-1 bg-slate-500 hover:bg-slate-600 cursor-pointer rounded font-bold text-xl'>-</button>
+                    <div className='flex flex-col justify-between py-3 w-25'>
+                        <div>{product.price.toLocaleString("pt-br", {
+                            style: "currency",
+                            currency: "BRL"
+                        })}</div>
+                        <div className=' flex gap-2 items-center'>
+                            <button className='px-2.5 py-1 bg-slate-500 hover:bg-slate-600 cursor-pointer rounded font-bold text-xl' onClick={() => addItemCart(product)}>+</button>
+                                    {product.amount}
+                            <button className='px-2.5 py-1 bg-slate-500 hover:bg-slate-600 cursor-pointer rounded font-bold text-xl'>-</button>
+                        </div>
                     </div>
-                </div>
 
-                <div className='flex flex-col justify-between py-3'>
-                    <div>
-                        <label className='font-semibold'> Total: </label>
-                         R$700
+                    <div className='flex flex-col justify-between py-3 ml-auto'>
+                        <div className='text-end'>
+                            <label className='font-semibold '>Subtotal: </label>
+                            {product.total.toLocaleString("pt-br", {
+                                style: "currency",
+                                currency: "BRL"
+                            })}
+                        </div>
+                        <RiDeleteBinLine size={24} className='ml-auto cursor-pointer'/> 
                     </div>
-                    <RiDeleteBinLine size={24} className='ml-auto cursor-pointer'/> 
-                </div>
-            </article>
+                </article>
+            )) }
         </div>
     )
 }
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,16 +1,34 @@
 import { CartItemsMobile } from '../../components/cartItemsMobile'
 import { useMediaQuery } from 'react-responsive'
 import { CartItemsDesktop } from '../../components/cartItemsDesktop'
+import { useContext } from 'react'; 
+import { CartContext } from '../../contexts/CartContext'
+import { Link } from 'react-router'
+import { HiArrowNarrowLeft } from 'react-icons/hi';
+
 
 
 export function Cart(){
 
     const isSmallScreen = useMediaQuery({ query: '(max-width: 768px)' });
+    const { cart, addItemCart } = useContext(CartContext);
+
 
     return(
         <section className="w-full max-w-7xl mx-auto px-4 py-18">
             <h1 className="font-semibold text-3xl text-slate-900 mb-10">Cart</h1>
-            { isSmallScreen ? <CartItemsMobile/> : <CartItemsDesktop/> }
+            { cart.length === 0 && (
+                <div className='flex flex-col w-fit m-auto p-2  justify-center items-center'>
+                    <p className='text-lg mb-2 text-slate-900 font-medium'>
+                        Seu carrinho está vazio :/
+                    </p>
+                    <Link className='flex items-center p-1.5 px-3 gap-2 text-amber-50 bg-slate-800 rounded-2xl' to={"/"}>
+                        <HiArrowNarrowLeft size={21}/>
+                        Voltar aos produtos
+                    </Link>
+                </div>
+            ) }
+            { isSmallScreen ? <CartItemsMobile cart={cart} addItemCart={addItemCart}/> : <CartItemsDesktop/> }
             
         </section>
     )
