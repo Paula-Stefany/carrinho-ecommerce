07e27719d9efd2d88d31be029be3286c00cbfa6a
Paula-Stefany <paula.sfa@outlook.com>
2025-05-01 17:12:47 -0300

    Concluído o footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import { FaSmile } from 'react-icons/fa';
+
+
+export function Footer(){
+    return(
+        <footer className="w-full py-2 bg-slate-900">
+            <p className='flex items-center text-amber-50 gap-3 w-fit m-auto'>Created by Paula Stéfany <FaSmile className='text-amber-50'/></p>
+        </footer>
+    )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,18 +2,19 @@ import { Banner } from '../../components/banner'
 import { Products } from '../../components/products'
 import { Banner2 } from '../../components/banner2'
 import { FrequentAskedQuestions } from '../../components/frequentAskedQuestions'
-
+import { Footer } from '../../components/footer'
 
 export function Home(){
 
-
     return(
-
-        <main className='w-full max-w-7xl m-auto'>
-            <Banner/>
-            <Products/>
-            <Banner2/>
-            <FrequentAskedQuestions/>
-        </main>
+        <>
+            <main className='w-full max-w-7xl m-auto'>
+                <Banner/>
+                <Products/>
+                <Banner2/>
+                <FrequentAskedQuestions/>
+            </main>
+            <Footer/>
+        </>
     )
 }
